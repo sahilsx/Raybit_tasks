f710e4344851767444089086ad239ed0c883a4f6
sahil <itxsaaho@gmail.com>
2024-09-26 10:49:29 +0530

    Stop tracking

--- Routes/userroutes.js
+++ Routes/userroutes.js
@@ -1,16 +1,16 @@
-// const  UserRouter = require("express").Router();
+const  UserRouter = require("express").Router();
 
-// const{
-//     signHandler,
-//     loginHandler,
-//     getusers
-// }=require("../controller/usercontroller/user"); 
+const{
+    signHandler,
+    loginHandler,
+    getusers
+}=require("../controller/usercontroller/user"); 
 
-// UserRouter.route("/",signHandler)
-//            .post("/signup",signHandler)
-//            .post("/login",loginHandler)
-//            .get("/get",getusers)
+UserRouter.route("/",signHandler)
+           .post("/signup",signHandler)
+           .post("/login",loginHandler)
+           .get("/get",getusers)
 
 
 
-// module.exports = UserRouter;
+module.exports = UserRouter;
